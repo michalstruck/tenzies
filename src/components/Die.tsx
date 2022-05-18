885acd8cf469180d72c9b9d969593927fefaec84
unknown <michal.o.struck@gmail.com>
2022-05-18 13:44:01 +0200

    fix: die rendering optimized a little

--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
 import "../styles/Die.css";
+
 interface PropTypes {
   value: number;
   isHeld: boolean;
@@ -10,63 +11,67 @@ export const Die = ({ value, isHeld, holdDice }: PropTypes) => {
   const renderedDots = useCallback(() => {
     let dots;
     let temp = Array(9).fill("");
-    if (value === 1) {
-      temp[4] = "·";
-      dots = temp.map((dot, i) => (
-        <div className={"die-filler"} key={i}>
-          {dot}
-        </div>
-      ));
-    } else if (value === 2) {
-      temp[0] = "·";
-      temp[8] = "·";
-      dots = temp.map((dot, i) => (
-        <div className={"die-filler"} key={i}>
-          {dot}
-        </div>
-      ));
-    } else if (value === 3) {
-      temp[2] = "·";
-      temp[4] = "·";
-      temp[6] = "·";
-      dots = temp.map((dot, i) => (
-        <div className={"die-filler"} key={i}>
-          {dot}
-        </div>
-      ));
-    } else if (value === 4) {
-      temp[0] = "·";
-      temp[2] = "·";
-      temp[6] = "·";
-      temp[8] = "·";
-      dots = temp.map((dot, i) => (
-        <div className={"die-filler"} key={i}>
-          {dot}
-        </div>
-      ));
-    } else if (value === 5) {
-      temp[0] = "·";
-      temp[2] = "·";
-      temp[4] = "·";
-      temp[6] = "·";
-      temp[8] = "·";
-      dots = temp.map((dot, i) => (
-        <div className={"die-filler"} key={i}>
-          {dot}
-        </div>
-      ));
+    if (value % 2 === 1) {
+      if (value === 1) {
+        temp[4] = "·";
+        dots = temp.map((dot, i) => (
+          <div className={"die-filler"} key={i}>
+            {dot}
+          </div>
+        ));
+      } else if (value === 3) {
+        temp[2] = "·";
+        temp[4] = "·";
+        temp[6] = "·";
+        dots = temp.map((dot, i) => (
+          <div className={"die-filler"} key={i}>
+            {dot}
+          </div>
+        ));
+      } else {
+        temp[0] = "·";
+        temp[2] = "·";
+        temp[4] = "·";
+        temp[6] = "·";
+        temp[8] = "·";
+        dots = temp.map((dot, i) => (
+          <div className={"die-filler"} key={i}>
+            {dot}
+          </div>
+        ));
+      }
     } else {
-      temp[0] = "·";
-      temp[2] = "·";
-      temp[3] = "·";
-      temp[5] = "·";
-      temp[6] = "·";
-      temp[8] = "·";
-      dots = temp.map((dot, i) => (
-        <div className={"die-filler"} key={i}>
-          {dot}
-        </div>
-      ));
+      if (value === 2) {
+        temp[0] = "·";
+        temp[8] = "·";
+        dots = temp.map((dot, i) => (
+          <div className={"die-filler"} key={i}>
+            {dot}
+          </div>
+        ));
+      } else if (value === 4) {
+        temp[0] = "·";
+        temp[2] = "·";
+        temp[6] = "·";
+        temp[8] = "·";
+        dots = temp.map((dot, i) => (
+          <div className={"die-filler"} key={i}>
+            {dot}
+          </div>
+        ));
+      } else {
+        temp[0] = "·";
+        temp[2] = "·";
+        temp[3] = "·";
+        temp[5] = "·";
+        temp[6] = "·";
+        temp[8] = "·";
+        dots = temp.map((dot, i) => (
+          <div className={"die-filler"} key={i}>
+            {dot}
+          </div>
+        ));
+      }
     }
     return dots;
   }, [value]);
