283c3303186d52d4c9104d4f472d5b786574a23c
unknown <michal.o.struck@gmail.com>
2022-05-19 17:57:39 +0200

    feat: added sessionStorage, fixed both Storages initialization

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -7,17 +7,23 @@ interface propTypes {
   firstRollOfGame: boolean;
 }
 
+// localStorage.clear();
+// sessionStorage.clear();
 let interval: NodeJS.Timer;
 
-if (
-  (localStorage.getItem("rollATB") || localStorage.getItem("timeATB")) &&
-  localStorage.getItem("timeATB") === "0" &&
-  localStorage.getItem("rollATB") === "0"
-) {
+if (!localStorage.getItem("rollATB") || !localStorage.getItem("timeATB")) {
   localStorage.setItem("rollATB", "");
   localStorage.setItem("timeATB", "");
 }
 
+if (!sessionStorage.getItem("rollSB") || !sessionStorage.getItem("timeSB")) {
+  sessionStorage.setItem("rollSB", "");
+  sessionStorage.setItem("timeSB", "");
+}
+
+console.log(sessionStorage, localStorage, +"");
+
+//fix localStorage and make sesh store
 export const Scoreboard = ({
   isGameFinished,
   rollNumber,
@@ -27,29 +33,45 @@ export const Scoreboard = ({
 
   useEffect(() => {
     if (isGameFinished) {
-      clearInterval(interval!);
-      setTime(0);
+      clearInterval(interval);
     } else if (rollNumber) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime + 1);
       }, 1000);
+    }
+    if (firstRollOfGame) {
+      setTime(0);
     }
 
     return () => clearInterval(interval);
   }, [firstRollOfGame, isGameFinished]);
 
   useEffect(() => {
-    if (
-      localStorage.getItem("rollATB")! > `${rollNumber}` &&
-      rollNumber !== 0
-    ) {
+    if (+localStorage.getItem("rollATB")! > rollNumber && rollNumber !== 0) {
+      localStorage.setItem("rollATB", `${rollNumber}`);
+    } else if (+localStorage.getItem("rollATB")! === 0) {
       localStorage.setItem("rollATB", `${rollNumber}`);
     }
 
-    if (localStorage.getItem("timeATB")! > `${time}` && time !== 0) {
+    if (+localStorage.getItem("timeATB")! > time && time !== 0) {
+      localStorage.setItem("timeATB", `${time}`);
+    } else if (+localStorage.getItem("timeATB")! === 0) {
       localStorage.setItem("timeATB", `${time}`);
     }
-    console.log("triggered localStorage change");
+  }, [isGameFinished]);
+
+  useEffect(() => {
+    if (+sessionStorage.getItem("rollSB")! > rollNumber && rollNumber !== 0) {
+      sessionStorage.setItem("rollSB", `${rollNumber}`);
+    } else if (+sessionStorage.getItem("rollSB")! === 0) {
+      sessionStorage.setItem("rollSB", `${rollNumber}`);
+    }
+
+    if (+sessionStorage.getItem("timeSB")! > time && time !== 0) {
+      sessionStorage.setItem("timeSB", `${time}`);
+    } else if (+sessionStorage.getItem("timeSB")! === 0) {
+      sessionStorage.setItem("timeSB", `${time}`);
+    }
   }, [isGameFinished]);
 
   return (
@@ -66,6 +88,12 @@ export const Scoreboard = ({
           {localStorage.getItem("timeATB")!.padStart(2, "0") +
             "s@" +
             localStorage.getItem("rollATB")!.padStart(2, "0") +
+            "rolls"}
+          <br />
+          Session's best:{" "}
+          {sessionStorage.getItem("timeSB")!.padStart(2, "0") +
+            "s@" +
+            sessionStorage.getItem("rollSB")!.padStart(2, "0") +
             "rolls"}
         </div>
       </div>
